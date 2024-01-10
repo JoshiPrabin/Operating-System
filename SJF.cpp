c617eb08092bb8907657ec407832ed401ee73fad
Prabin Joshi <prabinjoshi68@gmail.com>
2024-01-10 16:31:39 +0545

    Process scheduling using Shortest Job First algorithm

--- SJF.cpp
+++ SJF.cpp
@@ -1,70 +1,122 @@
 #include <iostream>
 #include <algorithm>
-
+#include <iomanip>
+#include <string.h>
 using namespace std;
 
-struct Process
+struct process
 {
-    int id;
-    int at;
-    int bt;
-    int ct;
-    int wt;
-    int tat;
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int start_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
 };
-
-bool comparebt(const Process &a, const Process &b)
-{
-    return a.bt < b.bt;
-}
 
 int main()
 {
+
     int n;
-    float awt = 0, atat = 0;
-    cout << "Enter the number of processes to be scheduled using SJF: ";
+    struct process p[20];
+    float avg_turnaround_time;
+    float avg_waiting_time;
+    int total_turnaround_time = 0;
+    int total_waiting_time = 0;
+    int total_response_time = 0;
+    int is_completed[100];
+    memset(is_completed, 0, sizeof(is_completed)); // using this to initialize array with zeros
+
+    cout << setprecision(2) << fixed;
+
+    cout << "Enter the number of processes: ";
     cin >> n;
 
-    Process processes[n];
-
-    cout << "Enter the arrival time of processes in order: " << endl;
     for (int i = 0; i < n; i++)
     {
-        processes[i].id = i + 1;
-        cin >> processes[i].at;
+        cout << "Enter arrival time of process " << i + 1 << ": ";
+        cin >> p[i].arrival_time;
+        cout << "Enter burst time of process " << i + 1 << ": ";
+        cin >> p[i].burst_time;
+        p[i].pid = i + 1;
+        cout << endl;
     }
+    int current_time = 0;
+    int completed = 0;
+    int prev = 0;
 
-    cout << "Enter the burst time of processes in order: " << endl;
+    while (completed != n)
+    {
+        int idx = -1;
+        int mn = 1000;
+        // for finding the shortest job
+        for (int i = 0; i < n; i++)
+        {
+            if (p[i].arrival_time <= current_time && is_completed[i] == 0)
+            {
+                if (p[i].burst_time < mn)
+                {
+                    mn = p[i].burst_time;
+                    idx = i;
+                }
+                if (p[i].burst_time == mn)
+                {
+                    if (p[i].arrival_time < p[idx].arrival_time)
+                    {
+                        mn = p[i].burst_time;
+                        idx = i;
+                    }
+                }
+            }
+        }
+        if (idx != -1)
+        {
+            p[idx].start_time = current_time;
+            p[idx].completion_time = p[idx].start_time + p[idx].burst_time;
+            p[idx].turnaround_time = p[idx].completion_time - p[idx].arrival_time;
+            p[idx].waiting_time = p[idx].turnaround_time - p[idx].burst_time;
+
+            total_turnaround_time += p[idx].turnaround_time;
+            total_waiting_time += p[idx].waiting_time;
+
+            is_completed[idx] = 1;
+            completed++;
+            current_time = p[idx].completion_time;
+            prev = current_time;
+        }
+        else
+        {
+            current_time++;
+        }
+    }
+    int min_arrival_time = 100;
+    int max_completion_time = -1;
     for (int i = 0; i < n; i++)
     {
-        cin >> processes[i].bt;
+        min_arrival_time = min(min_arrival_time, p[i].arrival_time);
+        max_completion_time = max(max_completion_time, p[i].completion_time);
     }
 
-    sort(processes, processes + n, comparebt);
+    avg_turnaround_time = (float)total_turnaround_time / n;
+    avg_waiting_time = (float)total_waiting_time / n;
+    cout << endl
+         << endl;
 
-    cout << "process\t at\t bt\t ct\t tat\t wt" << endl;
+    cout << "Process\t"
+         << "AT\t"
+         << "BT\t"
+         << "CT\t"
+         << "TAT\t"
+         << "WT\t"
+         << "\n"
+         << endl;
 
-    int ct = 0;
     for (int i = 0; i < n; i++)
     {
-        ct = ct + processes[i].bt;
-        processes[i].ct = ct;
-        processes[i].tat = processes[i].ct - processes[i].at;
-        processes[i].wt = processes[i].tat - processes[i].bt;
-
-        awt += processes[i].wt;
-        atat += processes[i].tat;
-
-        cout << processes[i].id << "\t " << processes[i].at << "\t " << processes[i].bt
-             << "\t " << ct << "\t " << processes[i].tat
-             << "\t " << processes[i].wt << endl;
+        cout << p[i].pid << "\t" << p[i].arrival_time << "\t" << p[i].burst_time << "\t" << p[i].completion_time << "\t" << p[i].turnaround_time << "\t" << p[i].waiting_time << "\n"
+             << endl;
     }
-
-    awt /= n;
-    atat /= n;
-
-    cout << "average waiting time = " << awt << endl;
-    cout << "average turn around time = " << atat << endl;
-
-    return 0;
+    cout << "Average Turnaround Time = " << avg_turnaround_time << endl;
+    cout << "Average Waiting Time = " << avg_waiting_time << endl;
 }
